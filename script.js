e2c07e7468b13cab9cf868b07f7ba4599a8a1e73
Valentyn Kisilenko <kisilenkovalentyn33@gmail.com>
2026-06-29 16:25:38 -0400

    hover on button added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const buttons = document.querySelectorAll('.button');
 const bottombarOrig = document.querySelector('.bottombar__orig');
 const bottombarShare = document.querySelector('.bottombar__share');
-const bottombareShareDesk = document.querySelector('.bottombar__share--desktop')
+const bottombareShareDesk = document.querySelector('.bottombar__share--desktop');
+const lightButton = document.querySelector('.button-image').parentElement;
+const darkButton = document.querySelector('.button-image-dark').parentElement;
 
 
 for(let i = 0; i < buttons.length; i++) {
@@ -10,6 +12,9 @@ for(let i = 0; i < buttons.length; i++) {
         bottombarShare.classList.toggle('hidden');
         } else {
         bottombareShareDesk.classList.toggle('hidden');
+        for (let y = 0; y < 2; y++) {
+            buttons[y].classList.toggle('hidden');
+        };
         }
 });
 };
@@ -18,6 +23,8 @@ window.addEventListener('resize', () => {
     bottombarOrig.classList.remove('hidden');
     bottombarShare.classList.add('hidden');
     bottombareShareDesk.classList.add('hidden');
+    lightButton.classList.remove('hidden');
+    darkButton.classList.add('hidden');
 });
 
 
@@ -44,5 +51,26 @@ document.addEventListener('click', (event) => {
         ) {
             bottombarShare.classList.add('hidden');
             bottombareShareDesk.classList.add('hidden');
+
+            if (window.innerWidth > 768)  {
+                lightButton.classList.remove('hidden');
+                darkButton.classList.add('hidden');
+            }
         }
+});
+
+lightButton.addEventListener('mouseenter', () => {
+    if (window.innerWidth > 768) {
+        lightButton.classList.add('hidden');
+        darkButton.classList.remove('hidden');
+    }
+});
+
+darkButton.addEventListener('mouseleave', () => {
+    if (window.innerWidth > 768 &&
+        bottombareShareDesk.classList.contains('hidden')) {
+
+        darkButton.classList.add('hidden');
+        lightButton.classList.remove('hidden');
+    }
 });
